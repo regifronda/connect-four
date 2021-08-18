55e978e2095682c3f76002eb07f3541c51537f9f
Regi Fronda <regisfronda@gmail.com>
2021-08-18 03:28:50 -0700

    Add in_choice_range? method

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,22 @@
 class Board
   attr_accessor :board
-  
+
   def initialize
     @board = Array.new(6) { Array.new(7) }
+  end
+
+  def render
+    puts
+    @board.each do |row|
+      row.each do |cell|
+      cell.nil? ? print("-") : print(cell.to_s)
+      end
+      puts
+    end
+    puts
+  end
+
+  def in_choice_range?(choice)
+    choice.between?(0, 6)
   end
 end
