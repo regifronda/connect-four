74b9417d751efc5e4d9400d6622eac63f2988a23
Regi Fronda <regisfronda@gmail.com>
2021-08-20 15:31:15 -0700

    Add winning_vertical? tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,7 +6,14 @@ describe Board do
 
     it 'creates a nested array for six rows and seven columns' do
       board = board_initialize.board
-      expect(board).to eq([[nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]])
+      expect(board).to eq([
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil], 
+        [nil, nil, nil, nil, nil, nil]])
     end
   end
 
@@ -30,12 +37,19 @@ describe Board do
     end
   end
 
-  describe 'column_choice_available?' do
+  describe '#column_choice_available?' do
     subject(:board_available) { described_class.new }
 
     context 'when given a choice for an empty column in the board and board is empty' do
       before do
-        board_available.instance_variable_set(:@board, [[nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]])
+        board_available.instance_variable_set(:@board, [
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil]])
       end
 
       it 'returns true' do
@@ -47,7 +61,13 @@ describe Board do
 
     context 'when given a choice for a nearly column in the board ' do
       before do
-        board_available.instance_variable_set(:@board, [["\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]])
+        board_available.instance_variable_set(:@board, [
+          ["\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil]])
       end
 
       it 'returns true' do
@@ -59,7 +79,13 @@ describe Board do
 
     context 'when given a choice for a full column in the board ' do
       before do
-        board_available.instance_variable_set(:@board, [["\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m"], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil]])
+        board_available.instance_variable_set(:@board, [
+          ["\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m", "\e[31mO\e[0m", "\e[37mO\e[0m"], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil]])
       end
 
       it 'returns false' do
@@ -69,4 +95,67 @@ describe Board do
       end
     end
   end
+
+  describe '#winning_vertical?' do
+    subject(:board_vertical) { described_class.new }
+
+    context 'when four of the same piece are vertically adjacent to one another' do
+      before do
+        board_vertical.instance_variable_set(:@board, [
+          ["\e[31mO\e[0m", "\e[31mO\e[0m", "\e[31mO\e[0m", "\e[31mO\e[0m", nil, nil],
+          ["\e[37mO\e[0m", "\e[37mO\e[0m", "\e[37mO\e[0m", nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil]
+          ])
+      end
+
+      it 'returns true' do
+        winning_piece = "\e[31mO\e[0m"
+        vertical_check = board_vertical.winning_vertical?(winning_piece)
+        expect(vertical_check).to be true
+      end
+    end
+
+    context 'when three of the same piece are vertically adjacent to one another' do
+      before do
+        board_vertical.instance_variable_set(:@board, [
+          ["\e[31mO\e[0m", "\e[31mO\e[0m", "\e[31mO\e[0m", nil, nil, nil],
+          ["\e[37mO\e[0m", "\e[37mO\e[0m", "\e[37mO\e[0m", nil, nil, nil], 
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil]
+          ])
+      end
+
+      it 'returns false' do
+        piece = "\e[31mO\e[0m"
+        vertical_check = board_vertical.winning_vertical?(piece)
+        expect(vertical_check).to be false
+      end
+    end
+
+    context 'when board is empty' do
+      before do
+        board_vertical.instance_variable_set(:@board, [
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil]
+          ])
+      end
+
+      it 'returns false' do
+        piece = "\e[31mO\e[0m"
+        vertical_check = board_vertical.winning_vertical?(piece)
+        expect(vertical_check).to be false
+      end
+    end
+  end
+
+  
 end
